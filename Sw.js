9a97e50afe269d907ca78f8fd784c481d736bcb0
yakov <dunetix@gmail.com>
2025-06-28 13:38:41 +0300

    Create Sw.js

---
+++ Sw.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("cryptoabuhay-cache").then(cache => {
+      return cache.addAll([
+        ".", "index.html", "style.css", "app.js", "manifest.json", "icon.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
